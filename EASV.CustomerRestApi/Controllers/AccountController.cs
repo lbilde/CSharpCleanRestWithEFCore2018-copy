266100f8b27c3ee708599ef4e9cc9f32e8669784
lbilde <ljb@easv.dk>
2018-11-17 23:35:39 +0100

    trying to add security with CLI

--- EASV.CustomerRestApi/Controllers/AccountController.cs
+++ EASV.CustomerRestApi/Controllers/AccountController.cs
@@ -13,7 +13,7 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace EASV.CustomerRestApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class AccountController : ControllerBase
     {
